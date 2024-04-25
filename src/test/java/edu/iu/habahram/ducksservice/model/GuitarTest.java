778df9b3c473eada588e6c88f77a3df7db9bdad7
LukeCali-949 <mengluke17@gmail.com>
2024-04-25 13:17:22 -0400

    test files

---
+++ src/test/java/edu/iu/habahram/ducksservice/model/GuitarTest.java
@@ -0,0 +1,20 @@
+package edu.iu.habahram.ducksservice.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuitarTest {
+
+    @Test
+    void getSerialNumber() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getTopWood() {
+    }
+}
---
+++ src/test/java/edu/iu/habahram/ducksservice/repository/InventoryRepositoryTest.java
@@ -0,0 +1,7 @@
+package edu.iu.habahram.ducksservice.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryRepositoryTest {
+
+}
